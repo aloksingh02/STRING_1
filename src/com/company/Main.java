c3c55f881e1365cf6aeeaf9a135c2f386f5a893b
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-03-13 22:25:52 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // create strings
+        String first = "java";
+        String second = "python";
+        String third = "javascript";
+
+        // print strings
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+    }
+}
